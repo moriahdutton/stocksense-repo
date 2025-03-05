151d8e8d9678c298f677195ac28299c277b89fc1
Moriah Dutton <moriahdutton@MacBookPro.attlocal.net>
2025-03-05 00:41:57 -0600

    StockSense xcode application file added

---
+++ StockSense/StockSense/StockSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSenseApp.swift
+//  StockSense
+//
+//  Created by Moriah Dutton on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct StockSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
